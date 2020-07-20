e191232c9c39655fb7b31e5b35ef62efffd9a75a
雨过初晴 <admin@choyeon.cn>
2020-07-20 14:56:42 +0800

    创建表单类 留言表单

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class MessageFrom(FlaskForm):
+    name = StringField('昵称', validators=[DataRequired(), Length(0, 20)])
+    body = TextAreaField('留言', validators=[DataRequired(), Length(0, 255)])
+    submit = SubmitField()
